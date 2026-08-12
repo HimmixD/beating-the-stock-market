e9ba5ecaf2b679a2d363ee89032170a492794ee9
Himmi <himmelias@gmail.com>
2026-08-12 18:50:45 +0200

    hopefully fixed venv problem and certainly fixed 20dfutures

--- src/quantstock/data/test_yfinance.py
+++ src/quantstock/data/test_yfinance.py
@@ -9,7 +9,9 @@ hist = ticker.history(start="2020-01-01", end=dt.datetime.now().strftime("%Y-%m-
 hist_raw = ticker.history(start="2020-01-01", end="2021-01-05", auto_adjust=False)
 
 hist["momentum_252d"] = hist["Close"].pct_change(periods=252)
-hist["future_return_20d"] = hist["Close"].shift(-20).pct_change(periods=20)
+hist["future_return_20d"] = hist["Close"].pct_change(periods=20).shift(-20)
+
+hist.dropna(inplace=True)
 
 
 
@@ -21,12 +23,6 @@ hist["future_return_20d"] = hist["Close"].shift(-20).pct_change(periods=20)
 #print(hist["Volume"].mean())
 #print(hist["Close"].min())
 #print(hist["Close"].max())
+print(hist)
 
-start_price = hist.loc["2020-01-02", "Close"]
-future_price = hist.iloc[20]["Close"]
-print(f"Start price: {start_price}")
-print(f"Future price: {future_price}")
-print(f"Future return: {future_price / start_price -1:.2%}")
-print(hist.loc["2020-01-02", "future_return_20d"])
-
-#hist.to_csv("data/raw/AAPL_price.csv")
+hist.to_csv("data/raw/AAPL_price.csv")
